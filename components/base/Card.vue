b7f36134cce12860533a8740ab820a2729254617
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2021-12-17 01:43:34 +0900

    [add] #4 BaseCardの追加

---
+++ components/base/Card.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="w-fit h-fit m-5">
+		<nuxt-link :to="'/post/' + id" class="w-fit h-fit m-0">
+			<div
+				class="w-96 h-44 rounded-2xl px-4 py-1 shadow-lg m-0"
+				:class="bgColor"
+			>
+				<div
+					class="h-5/6 pb-3 pt-3 break-words text-ms text-white flex items-center"
+				>
+					<span class="w-full h-fit overflow-hidden line-clamp-5">
+						<slot />
+					</span>
+				</div>
+				<div class="flex justify-between items-center text-xs">
+					<div class="text-slate-300 px-3">{{ date }}</div>
+					<div class="flex justify-ennd">
+						<CategoryTag v-for="tag in tags" :key="tag.id">
+							<div>{{ tag.text }}</div>
+						</CategoryTag>
+					</div>
+				</div>
+			</div>
+		</nuxt-link>
+	</div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+	bgColor: String,
+	id: String,
+	date: String,
+	tags: Array(Object),
+})
+</script>
