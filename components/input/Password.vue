ced9461690436e55dd70ae0bf2c5595296c83d2d
pikake <maha.jasminum@gmail.com>
2021-12-18 00:19:56 +0900

    #10 InputPassword inputのみに修正

--- components/input/Password.vue
+++ components/input/Password.vue
@@ -1,7 +1,5 @@
 <template>
     <div>
-        <form size="30" maxlength="30" class="border rounded border-black w-full h-12 px-3">
-            <input type="password" placeholder="パスワード"> 
-        </form>
+        <input class="border rounded border-black w-full h-12 px-3" type="password" placeholder="パスワード" />
     </div>
 </template>
