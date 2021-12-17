a13a88836bc02dacf72b8b0570d1bceb2982fbd5
pikake <maha.jasminum@gmail.com>
2021-12-17 22:33:42 +0900

    #3 baseButtonの作成

---
+++ components/base/Button.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="base-button">
+        <button class="bg-amber-200 text-black border border-black font-sans shadow-md rounded hover:bg-amber-300"><slot /></button>
+    </div>
+</template>
