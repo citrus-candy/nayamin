3d3eb637fc5e1afba64b1dd8781e99550517aa4a
Simo-C3 <65708479+Simo-C3@users.noreply.github.com>
2021-12-17 20:21:11 +0900

    [change] MRのコメント対応

--- components/base/Card.vue
+++ components/base/Card.vue
@@ -1,12 +1,21 @@
+<script lang="ts" setup>
+defineProps({
+	bgColor: String,
+	id: String,
+	date: String,
+	tags: Object,
+})
+</script>
+
 <template>
 	<div class="w-fit h-fit m-5">
-		<nuxt-link :to="'/post/' + id" class="w-fit h-fit m-0">
+		<nuxt-link :to="`/post/${id}`" class="w-fit h-fit m-0">
 			<div
 				class="w-96 h-44 rounded-2xl px-4 py-1 shadow-lg m-0"
 				:class="bgColor"
 			>
 				<div
-					class="h-5/6 pb-3 pt-3 break-words text-ms text-white flex items-center"
+					class="h-5/6 py-3 break-words text-ms text-white flex items-center"
 				>
 					<span class="w-full h-fit overflow-hidden line-clamp-5">
 						<slot />
@@ -14,8 +23,8 @@
 				</div>
 				<div class="flex justify-between items-center text-xs">
 					<div class="text-slate-300 px-3">{{ date }}</div>
-					<div class="flex justify-ennd">
-						<CategoryTag v-for="tag in tags" :key="tag.id">
+					<div class="flex justify-end">
+						<CategoryTag class="mx-1" v-for="tag in tags" :key="tag.id">
 							<div>{{ tag.text }}</div>
 						</CategoryTag>
 					</div>
@@ -25,11 +34,3 @@
 	</div>
 </template>
 
-<script lang="ts" setup>
-const props = defineProps({
-	bgColor: String,
-	id: String,
-	date: String,
-	tags: Array(Object),
-})
-</script>
