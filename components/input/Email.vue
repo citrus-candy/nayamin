8a6df56352bab21b4e13b37a383867eb55aaf759
Simo-C3 <65708479+Simo-C3@users.noreply.github.com>
2021-12-17 22:40:19 +0900

    [add] #9 InputEmailの作成

---
+++ components/input/Email.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <input
+        type="email"
+        id="email"
+        pattern="^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$"
+        size="30"
+        maxlength="30"
+        autocomplete="on"
+        required
+        placeholder="メールアドレス"
+        class="border rounded border-black w-full h-12 px-3"
+      >
+  </div>
+</template>
