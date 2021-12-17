7ae5e974c315ed002c654b747cd631cdb47f0143
pikake <maha.jasminum@gmail.com>
2021-12-17 23:53:16 +0900

    #10 InputPasswordの作成

---
+++ components/input/Password.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <form size="30" maxlength="30" class="border rounded border-black w-full h-12 px-3">
+            <input type="password" placeholder="パスワード"> 
+        </form>
+    </div>
+</template>
