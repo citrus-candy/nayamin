b796215f7c8c6589dbadaa2d2ce9eb45a1db8dfc
Simo-C3 <65708479+Simo-C3@users.noreply.github.com>
2021-12-17 21:20:06 +0900

    [add] #6 スライダーの作成

---
+++ components/base/Slider.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <input type="range" name="slider" id="hoge" min="1" max="10" step="1" value="5"
+    class="w-full cursor-pointer appearance-none bg-slate-800 h-0.5"
+    >
+  </div>
+</template>
