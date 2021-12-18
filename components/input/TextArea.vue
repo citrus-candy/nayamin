9d40751d285edb5636e90e444163562794ea9e37
kyuuri11013 <kyuuri11013@gmail.com>
2021-12-18 22:48:51 +0900

    [fix] PostCardまわりの修正 #40

--- components/input/TextArea.vue
+++ components/input/TextArea.vue
@@ -1,7 +1,11 @@
+<script setup lang="ts">
+const text = ref('')
+</script>
+
 <template>
 	<div>
 		<textarea
-			onkeyup="document.getElementById('count').innerText=this.value.length"
+			v-model="text"
 			autofocus
 			cols="50"
 			rows="10"
@@ -10,6 +14,6 @@
 			required
 			class="w-full h-full break-words border border-black p-4"
 		></textarea>
-		<p class="text-right mr-3"><span id="count"></span>/500</p>
+		<p class="text-right mr-3">{{ text.length }} / 500</p>
 	</div>
 </template>
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="flex justify-center items-center h-screen flex-col px-20">
 		<div class="w-full max-w-5xl">
-			<PostCard></PostCard>
+			<PostCard />
 		</div>
 		<div class="flex justify-center">
 			<BaseButton class="text-4xl py-4 px-8 mt-12">相談する</BaseButton>
