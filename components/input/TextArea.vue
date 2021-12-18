eeb544e14bccdd44ffc517badd0f86e17d3aca2b
kyuuri11013 <kyuuri11013@gmail.com>
2021-12-19 00:53:45 +0900

    [add] お悩みの投稿処理を実装 #43

--- components/input/TextArea.vue
+++ components/input/TextArea.vue
@@ -1,19 +1,33 @@
-<script setup lang="ts">
-const text = ref('')
+<script lang="ts">
+import { useVModel } from 'vue-composable'
+
+export default defineComponent({
+	props: {
+		value: String,
+		lengthTextColor: String,
+	},
+	setup(props) {
+		const value = useVModel(props, 'value')
+
+		return { value }
+	},
+})
 </script>
 
 <template>
 	<div>
 		<textarea
-			v-model="text"
+			v-model="value"
 			autofocus
 			cols="50"
 			rows="10"
 			maxlength="500"
 			placeholder="ここに悩みを書く"
 			required
-			class="w-full h-full break-words border border-black p-4"
+			class="w-full h-full break-words border border-black p-4 focus:outline-none"
 		></textarea>
-		<p class="text-right mr-3">{{ text.length }} / 500</p>
+		<p class="text-right mr-3" :class="lengthTextColor">
+			{{ value.length }} / 500
+		</p>
 	</div>
 </template>
