4e0364721ce745bdf7fc8b6530669bf124084019
Simo-C3 <65708479+Simo-C3@users.noreply.github.com>
2021-12-18 13:32:41 +0900

    [add] #14 PostCardの追加

--- plugins/font-awesome.ts
+++ plugins/font-awesome.ts
@@ -1,9 +1,9 @@
 import { defineNuxtPlugin } from '#app'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faLeaf } from '@fortawesome/free-solid-svg-icons'
+import { faLeaf, faTag } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faLeaf)
+library.add(faLeaf, faTag)
 
 export default defineNuxtPlugin((nuxtApp) => {
 	return nuxtApp.vueApp.component('FontAwesomeIcon', FontAwesomeIcon)
