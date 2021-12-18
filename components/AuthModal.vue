0202c2638a7925a1629413a2f30f1be675615334
kyuuri11013 <kyuuri11013@gmail.com>
2021-12-18 22:07:56 +0900

    [add] MenuBarの各種機能の実装 #38

--- components/AuthModal.vue
+++ components/AuthModal.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
 import { FirebaseError } from 'firebase/app'
 
+type Emits = {
+	(e: 'closeModal'): void
+}
+
 const showSignUpModal = ref(false)
 const email = ref('')
 const password = ref('')
+const successMessage = ref('')
 const errorMessage = ref('')
 
+const emit = defineEmits<Emits>()
 const signIn = () => {
 	errorMessage.value = ''
 	_signIn(email.value, password.value)
 		.then(() => {
-			console.log('ログインに成功しました')
+			emit('closeModal')
 		})
 		.catch((error: FirebaseError) => {
 			if (error.code === 'auth/user-not-found') {
@@ -24,30 +30,42 @@ const signIn = () => {
 }
 
 const signUp = () => {
+	successMessage.value = ''
 	errorMessage.value = ''
 	_signUp(email.value, password.value)
 		.then(() => {
 			console.log('新規登録に成功しました')
+			successMessage.value = '新規登録に成功しました'
 		})
 		.catch((error) => {
 			if (error.code === 'auth/invalid-email') {
 				errorMessage.value = 'そのメールアドレスは無効です'
 			} else if (error.code === 'auth/invalid-password') {
 				errorMessage.value = 'パスワードは6文字以上にしてください'
+			} else if (error.code === 'auth/email-already-in-use') {
+				errorMessage.value = 'そのメールアドレスはすでに使われています'
 			} else {
 				errorMessage.value = error.message
 			}
 		})
 }
+
+watch(showSignUpModal, () => {
+	successMessage.value = ''
+	errorMessage.value = ''
+})
 </script>
 
 <template>
 	<div
-		class="w-screen h-screen fixed bg-slate-200 bg-opacity-50 flex justify-center items-center"
+		class="w-screen h-screen fixed top-0 bg-slate-200 bg-opacity-50 flex justify-center items-center"
 	>
 		<div class="w-96 rounded-xl border border-black bg-white">
 			<div v-if="!showSignUpModal">
-				<button class="text-left px-2 hover:bg-slate-100 rounded-tl-xl">
+				<button
+					class="text-left px-2 hover:bg-slate-100 rounded-tl-xl"
+					@click="emit('closeModal')"
+				>
 					<FontAwesomeIcon icon="times" class="w-10 h-10" />
 				</button>
 				<hr />
@@ -83,6 +101,7 @@ const signUp = () => {
 					<BaseButton class="mb-4" @click="signUp">
 						<p class="px-10 py-1 text-lg">新規登録</p>
 					</BaseButton>
+					<p class="text-green-500">{{ successMessage }}</p>
 					<p class="text-red-500">{{ errorMessage }}</p>
 				</div>
 			</div>
--- components/MenuBar.vue
+++ components/MenuBar.vue
@@ -1,42 +1,50 @@
 <script setup lang="ts">
+import { useAuthState } from '~~/composables/auth'
+
 type MenuBarContents = {
 	title: string
 	icon: string
 	color: string
+	to: string
 }
 
+const authState = useAuthState()
+
 const showBar = ref(false)
+const showAuthModal = ref(false)
 const menuBarContents: MenuBarContents[] = [
-	{
-		title: 'ログイン',
-		icon: 'sign-in-alt',
-		color: 'text-blue-400',
-	},
 	{
 		title: '悩みを聞いてみる',
 		icon: 'comment-dots',
 		color: 'text-cyan-400',
+		to: '/',
 	},
 	{
 		title: '悩みを打ち明ける',
 		icon: 'comments',
 		color: 'text-amber-400',
+		to: '/post',
 	},
 	{
 		title: 'ランキング',
 		icon: 'crown',
 		color: 'text-yellow-400',
-	},
-	{
-		title: 'ログアウト',
-		icon: 'sign-out-alt',
-		color: 'text-red-500',
+		to: 'ranking',
 	},
 ]
+const signOut = () => {
+	_signOut()
+		.then(() => {
+			console.log('ログアウトに成功しました')
+		})
+		.catch((error) => {
+			console.log(error)
+		})
+}
 </script>
 
 <template>
-	<div>
+	<div class="bg-white">
 		<div v-if="!showBar" class="w-16 border-r border-black h-screen">
 			<button class="w-full py-1 hover:bg-slate-100" @click="showBar = true">
 				<FontAwesomeIcon icon="bars" class="w-10 h-10" />
@@ -51,24 +59,40 @@ const menuBarContents: MenuBarContents[] = [
 				</button>
 			</div>
 			<hr />
-			<div v-for="(content, i) in menuBarContents" :key="i">
-				<button class="w-full py-4 px-5 flex items-center hover:bg-slate-100">
-					<FontAwesomeIcon
-						:icon="content.icon"
-						class="w-6 h-6"
-						:class="content.color"
-					/>
-					<p
-						class="ml-3"
-						:class="{
-							[content.color]: i === 0 || i === menuBarContents.length - 1,
-						}"
-					>
-						{{ content.title }}
-					</p>
-				</button>
-				<hr v-if="i === 0 || i === menuBarContents.length - 2" />
-			</div>
+			<nuxt-link
+				v-for="(content, i) in menuBarContents"
+				:key="i"
+				class="w-full py-4 px-5 flex items-center hover:bg-slate-100"
+				:to="content.to"
+				@click="showBar = false"
+			>
+				<FontAwesomeIcon
+					:icon="content.icon"
+					class="w-6 h-6"
+					:class="content.color"
+				/>
+				<p class="ml-3">
+					{{ content.title }}
+				</p>
+			</nuxt-link>
+			<hr />
+			<button
+				v-if="!authState"
+				class="w-full py-4 px-5 flex items-center hover:bg-slate-100"
+				@click="showAuthModal = true"
+			>
+				<FontAwesomeIcon icon="sign-in-alt" class="w-6 h-6 text-blue-400" />
+				<p class="ml-3 text-blue-400">ログイン</p>
+			</button>
+			<button
+				v-else
+				class="w-full py-4 px-5 flex items-center hover:bg-slate-100"
+				@click="signOut"
+			>
+				<FontAwesomeIcon icon="sign-out-alt" class="w-6 h-6 text-red-500" />
+				<p class="ml-3 text-red-500">ログアウト</p>
+			</button>
 		</div>
+		<AuthModal v-if="showAuthModal" @close-modal="showAuthModal = false" />
 	</div>
 </template>
