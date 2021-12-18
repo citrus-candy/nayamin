5e930cea9ae952e2adc7e0421ddf3921b0a3003a
kyuuri11013 <kyuuri11013@gmail.com>
2021-12-18 13:30:36 +0900

    [change] 文字数カウントの処理を変更 #15

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const text = ref('')
+</script>
+
+<template>
+	<div>
+		<textarea
+			v-model="text"
+			autofocus
+			cols="50"
+			rows="10"
+			maxlength="500"
+			placeholder="ここに悩みを書く"
+			required
+			class="w-full h-full break-words border border-black p-4"
+		></textarea>
+		<p class="text-right mr-3">{{ text.length }} / 500</p>
+	</div>
+</template>
