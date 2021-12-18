2e314cb28285dedadb3058d857c7c1f95a42b089
kyuuri11013 <kyuuri11013@gmail.com>
2021-12-18 13:54:53 +0900

    [fix] ソースコードのリファクタリング #11

--- components/post/AnswerCard.vue
+++ components/post/AnswerCard.vue
@@ -1,29 +1,13 @@
 <script lang="ts" setup>
 defineProps({
-  answerCardTime: String,
+  text: String,
+  time: String,
 })
 </script>
 
 <template>
-  <div class="answer-card-wrapper">
-    <div class="answer-card w-full shadow-lg rounded px-4 pt-6 pb-12">
-      <slot />
-    </div>
-    <div class="answer-card-time text-gray-300">{{ answerCardTime }}</div>
+  <div class="w-full shadow-lg rounded p-4 border-l-8 border-pink-300">
+    <p class="h-24 min-h-full">{{ text }}</p>
+    <p class="text-gray-300 text-right">{{ time }}</p>
   </div>
 </template>
-
-<style lang="css">
-.answer-card-wrapper {
-  position: relative;
-}
-
-.answer-card {
-  border-left: 6px solid rgb(236, 177, 190);
-}
-.answer-card-time {
-  position: absolute;
-  bottom: 8px;
-  right: 28px;
-}
-</style>
