ee735c8b0bda5288e750c705ef43947489618133
pikake <maha.jasminum@gmail.com>
2021-12-19 14:22:12 +0900

    #55 AnswerModalのアニメーション追加

--- components/AnswerModal.vue
+++ components/AnswerModal.vue
@@ -9,7 +9,7 @@ const emit = defineEmits<Emits>()
 
 <template>
 	<div
-		class="fixed bottom-[-2px] right-5 shadow-gray-500 shadow-2xl w-3/5 m-auto p-10 border border-black rounded bg-white z-10"
+		class="answer-modal fixed bottom-[-2px] right-5 shadow-gray-500 shadow-2xl w-3/5 m-auto p-10 border border-black rounded bg-white z-10"
 	>
 		<InputTextArea
 			v-model:value="text"
@@ -26,3 +26,19 @@ const emit = defineEmits<Emits>()
 		</div>
 	</div>
 </template>
+
+<style>
+.answer-modal {
+	animation: SlideIn 0.5s;
+}
+@keyframes SlideIn {
+	0% {
+		opacity: 0;
+		transform: translateY(64px);
+	}
+	100% {
+		opacity: 1;
+		transform: translateY(0);
+	}
+}
+</style>
