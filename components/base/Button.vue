71074bf7224a1403cf6d8862a85707526209ef85
Simo-C3 <65708479+Simo-C3@users.noreply.github.com>
2021-12-19 14:02:37 +0900

    [add] add animation to BaseButton

--- components/base/Button.vue
+++ components/base/Button.vue
@@ -1,6 +1,6 @@
 <template>
 	<button
-		class="bg-amber-200 text-black border border-black font-sans shadow-md rounded hover:bg-amber-300"
+		class="bg-amber-200 text-black border border-black font-sans shadow-md rounded hover:bg-amber-300 hover:shadow-lg"
 	>
 		<slot />
 	</button>
